cf30bca66d0ebeeec1d98481dea4073428bafb2a
Joan Magno <magnojoan@gmail.com>
2013-04-09 13:41:46 +0800

    All

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asis::Application.config.secret_token = 'f4b15c9a6def35f30c44bec7d094293809fb521a6b7bec3f221defe832215e338f5f23b20b954234a5fd485ba2d418eb66cc9d5c4bcded8452f0d36f4ed8ba19'
