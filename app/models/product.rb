a294e3ea481e689dcb382f7c8c2105691fed44f6
Joan Magno <magnojoan@gmail.com>
2013-04-13 21:12:51 +0800

    Hapsters

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,6 @@ class Product < ActiveRecord::Base
   has_many :adjustments
 
    validates :product_code, :presence => true
-   validates :product_version, :presence => true
    validates :product_name, :presence => true
    validates :reorder_level, :presence => true, :numericality => true
    validates :effective_date, :presence => true
@@ -19,11 +18,16 @@ class Product < ActiveRecord::Base
   before_save :add_version
 
 def add_version
-    # since you're creating a new product record even on update
-    self.id = nil
-    previous_product_version = Product.first # or Product.last. I forgot. lol
+   
+    check = Product.first
+    if check.blank?
+      self.product_version = 1
+    else
+    previous_product_version = Product.first.product_version
     self.product_version = previous_product_version + 1
+  end
 end
 
 
 end
+
